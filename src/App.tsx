2eb3d69eddd53a56b04178f042148b07d7eb2d70
billiejoenahema <billiejoenahema@gmail.com>
2021-04-29 11:02:18 +0900

    implement: create task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import {FormControl, TextField} from '@material-ui/core'
 import React, {useState, useEffect} from 'react'
 import './App.css'
 import { db } from './firebase'
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos'
 
 const App: React.VFC = () => {
   const [tasks, setTasks] = useState([{id: '', title: ''}])
+  const [inputTask, setInputTask] = useState('')
   useEffect(() => {
     const unSubscribe = db.collection('tasks').onSnapshot((snapshot) => {
       setTasks(
@@ -13,9 +16,27 @@ const App: React.VFC = () => {
     return () => unSubscribe()
   },[])
 
+  const createTask = (e: React.MouseEventHandler<HTMLButtonElement>) => {
+    db.collection('tasks').add({title: inputTask})
+    setInputTask('')
+  }
+
   return (
     <div className="App">
-      {tasks.map((task) => <h3>{task.title}</h3>)}
+      <h1>Todo App by React/Firebase</h1>
+      <FormControl>
+        <TextField
+          label="New task?"
+          value={inputTask}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInputTask(e.target.value)
+          }
+        />
+      </FormControl>
+      <button disabled={!inputTask} onClick={createTask}>
+        <AddToPhotosIcon />
+      </button>
+      {tasks.map((task) => <h3 key={task.id}>{task.title}</h3>)}
     </div>
   )
 }
